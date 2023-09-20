41680d1df83a31c42b0e7207e883638eda56ade3
pantq1711 <hongan.gv10@gmail.com>
2023-09-20 22:29:45 +0700

    src code

--- J02016.java
+++ J02016.java
@@ -1,38 +1,37 @@
 import java.util.*;
 
 public class J02016 {
-    public static boolean search ( int j, int n, int arr[], int tim) {
-        int l=j, r=n-1;
-        while(l<=r) {
-            int mid=(r+l)/2;
-            if(tim==arr[mid]) return true;
-            if(tim>arr[mid]) l=mid+1;
-            else r=mid-1;
-        }
-        return false;
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while (t-- > 0) {
-            int n = sc.nextInt(), ok = 0;
+            int n = sc.nextInt();
             long[] a = new long[n];
             for (int i = 0; i < n; i++) {
                 a[i] = sc.nextLong();
             }
-            Arrays.sort(a);
-            for (int i = 0; i < n-2; i++) {
-                for (int j = i + 1; j < n-1; j++) {
-                    long k = (long)( a[i] * a[i] )+ (long)( a[j] * a[j]);
-                    if(search(j+1,n, a,(long)sqrt(tmp))){
-                        check = 1;
-                        printf("YES\n");
+
+            Set<Long> squareSet = new HashSet<>();
+            for (int i = 0; i < n; i++) {
+                squareSet.add(a[i] * a[i]);
+            }
+
+            boolean found = false;
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    long sumSquare = a[i] * a[i] + a[j] * a[j];
+                    if (squareSet.contains(sumSquare)) {
+                        found = true;
                         break;
                     }
                 }
+                if (found) {
+                    break;
+                }
             }
-            System.out.println(ok == 1 ? "YES" : "NO");
+
+            System.out.println(found == true ? "YES" : "NO");
         }
         sc.close();
     }
---
+++ JKT015.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Anphan
+ */
+import java.util.*;
+public class JKT015 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        Stack <Character> st1 = new Stack<>();
+        Stack <Character> st2 = new Stack<>();
+        for(int i=0; i<s.length(); ++i)
+        {
+            if(s.charAt(i) == '-'){
+                if(!st1.empty()) st1.pop();
+            }
+            else if(s.charAt(i) == '<'){
+                if(!st1.empty()){
+                    st2.push(st1.peek());
+                    st1.pop();
+                }
+            }
+            else if(s.charAt(i) == '>'){
+                if(!st2.empty()){
+                    st1.push(st2.peek());
+                    st2.pop();
+                }
+            }
+            else st1.push(s.charAt(i));
+        }
+        while(!st1.empty())
+        {
+            st2.push(st1.peek());
+            st1.pop();
+        }
+        while(!st2.empty())
+        {
+            System.out.print(st2.peek());
+            st2.pop();
+        }
+    }
+}
